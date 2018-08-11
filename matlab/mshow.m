1f7663da85474121fcf09fd6738a472ff5e9c818
Ales Pecka <alespecka1@gmail.com>
2018-08-11 15:50:14 +0200

    new matlab script mshow for visualisation of 2D simulations

---
+++ matlab/mshow.m
@@ -0,0 +1,78 @@
+function mshow(varargin)
+
+[meshPath, simulPath, outputPath] = getPath;
+
+par = loadParam(simulPath);
+vertices = load([outputPath,'vertices.txt']);
+if par.dimension ~= 2 && size(vertices,2) ~= 2
+    error('mshow supports only 2D view');
+end
+elements = dlmread([meshPath, 'elements.txt'], ' ');
+elementType = load([meshPath, 'elementType.txt']);
+
+tri = convert2Triangular(elements, elementType);
+
+show(varargin{:});
+
+for k = 1 : nargin
+    q = varargin{k};
+
+    if q(1) == '-'
+        warning('cannot evaluate option ''%s'': switches are not allowed here', q)
+        continue
+    end
+    
+    plotMe(q)
+end
+
+function plotMe(quantityName)
+    filePath = [outputPath, quantityName, '.txt'];
+    
+    if ~exist(filePath, 'file')
+        warning('cannot find file %s', filePath)
+    end
+    
+    quantity = load(filePath);
+    m = size(quantity, 2);
+    if m == 1
+        myContour(tri, vertices, quantity, quantityName);
+    elseif m > 2
+        figure('name', quantityName, 'color', 'w');
+        quiver(vertices(:,1), vertices(:,2), quantity(:,1), quantity(:,2));
+        axis equal
+    end
+end
+
+end
+
+function myContour(tri, vertices, Quantity, name)
+    figure('name', name, 'color', 'w');
+    [~, h2] = tricontf(vertices(:,1),vertices(:,2),tri+1,Quantity,30);
+    % tricontour(tri,PX,PY,Quantity,30)
+    set(h2, 'linestyle', 'none');
+    box on;
+    axis equal;
+    osy = [min(vertices(:,1)) max(vertices(:,1)) min(vertices(:,2)) max(vertices(:,2))];
+    axis(osy);
+    colorbar;
+    colormap jet
+end
+
+% splits any quadriliteral elements into two triangles for the
+% visualisation purposes
+function tri = convert2Triangular(elements, elementType)
+
+    nElems = size(elements, 1);
+    
+    if nargin > 1
+        squares = elements(elementType == 4, :);        
+    else
+        squares = elements(elements(:,4) ~= 0, :);
+    end
+    
+    tri = zeros(nElems + length(squares), 3);
+    tri(1:nElems, :) = elements(:,1:3);
+    for i = 1 : length(squares)
+        tri(nElems+i, :) = [squares(i,1) squares(i,3) squares(i,4)];
+    end    
+end
--- matlab/show.m
+++ matlab/show.m
@@ -11,83 +11,10 @@ function show(varargin)
     %
     %   show mach pressure -i100 -ftxt
     
-    str = 'java -d64 -Xmx8g -jar FlowPro.jar postprocessing ';
-    for i = 1:nargin
-        str = [str,' ', varargin{i}];
-    end
-
+    str = ['java -d64 -Xmx8g -jar FlowPro.jar postprocessing ', varargin{:}];
+    
     path = pwd;
     cd(getFlowProPath);
     system(str);
     cd(path)
-
-    try
-        [meshPath, simulPath, outputPath] = getPath;
-        vertices = load([outputPath,'vertices.txt']);
-        if(size(vertices,2) > 2)
-            return;
-        end
-        elements = nactiMat([meshPath,'elements.txt']);
-        elementType = load([meshPath,'elementType.txt']);
-
-        % tvorba triangulace
-        ne = length(elements(:,1));
-        tri = [];
-        for i = 1:ne
-            for j = 2:elementType(i)-1
-                tri = [tri;elements(i,[1,j,j+1])];
-            end
-        end    
-
-        for i = 1:nargin
-            if(varargin{i}(1) ~= '-')
-                variableName = varargin{i};
-                quantity = load([outputPath,[variableName,'.txt']]);
-                myContour(tri, vertices, quantity, variableName);
-            end
-        end
-    catch
-%         close all;
-%         display('Unable to show results in matlab!');
-    end
-end
-
-
-function myContour(tri, vertices, Quantity, name)
-    figure('name', name, 'color', 'w');
-    [h1, h2] = tricontf(vertices(:,1),vertices(:,2),tri+1,Quantity,30);
-    % tricontour(tri,PX,PY,Quantity,30)
-    set(h2, 'linestyle', 'none');
-    box on;
-    axis equal;
-    osy = [min(vertices(:,1)) max(vertices(:,1)) min(vertices(:,2)) max(vertices(:,2))];
-    axis(osy);
-    colorbar;
-    colormap jet
-end
-
-
-function TP = nactiMat(str)
-    fid = fopen(str,'r');
-    n = 0;
-    max = 0;
-    while(feof(fid) == 0)
-        line = fgetl(fid);
-        v = str2num(line);
-        if(length(v) > max)
-            max = length(v);
-        end
-        n = n + 1;
-    end
-    fclose(fid);
-    TP = zeros(n,max);
-    fid = fopen(str,'r');
-    n = 1;
-    while(feof(fid) == 0)
-        line = fgetl(fid);
-        v = str2num(line);
-        TP(n,1:length(v)) = v;
-        n = n + 1;
-    end
-    fclose(fid);
 end
