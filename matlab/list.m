1ad044608ddc678c2a2da13289c5d9c16255be2d
Ales Pecka <alespecka1@gmail.com>
2018-08-11 16:26:53 +0200

    bug in the script list.m fixed - geometries in subdirs are shown correctly

--- matlab/list.m
+++ matlab/list.m
@@ -1,19 +1,35 @@
 function list
 flowProPath = getFlowProPath;
 
-geomList = dir([flowProPath,'/simulations']);
-for i = 1 : length(geomList)
-    if geomList(i).isdir && ~strcmp(geomList(i).name, '.') && ~strcmp(geomList(i).name , '..')
-        fprintf(1, '%s: ', geomList(i).name);
-        
-        simulList = dir(strcat(flowProPath,'/simulations/', geomList(i).name));
-        for j = 1 : length(simulList)
-            if simulList(j).isdir && ~strcmp(simulList(j).name, '.') ...
-                    && ~strcmp(simulList(j).name , '..') && ~strcmp(simulList(j).name , 'mesh')
-                fprintf(1, '%s ', simulList(j).name);
-            end
+searchDir([flowProPath,'/simulations'], '');
+
+end
+
+function searchDir(absPath, relPath)
+    dirContent = dir(absPath);
+
+    for i = 1 : length(dirContent)
+        if dirContent(i).isdir && ~strcmp(dirContent(i).name, '.') && ~strcmp(dirContent(i).name , '..')
+            name = dirContent(i).name;
+            if exist([relPath, name, '/', 'mesh'], 'dir')
+                fprintf(1, '%s%s: ', relPath, name);
+                searchGeom([dirContent(i).folder, '/', name])
+            else
+                searchDir([dirContent(i).folder, '/', name], [relPath, name, '/']);
+            end            
         end
-        
-        fprintf(1, '\n');
     end
+end
+
+function searchGeom(absPath)
+
+    simulList = dir(absPath);
+    for j = 1 : length(simulList)
+        if simulList(j).isdir && ~strcmp(simulList(j).name, '.') ...
+                && ~strcmp(simulList(j).name , '..') && ~strcmp(simulList(j).name , 'mesh')
+            fprintf(1, '%s ', simulList(j).name);
+        end
+    end
+
+    fprintf(1, '\n');
 end
