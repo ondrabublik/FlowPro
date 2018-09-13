4ad9c840da3e71500d51bd5d12785f3548193cdf
Ales Pecka <alespecka1@gmail.com>
2018-09-13 11:30:10 +0200

    Move showResiduum into mshow and add plotting of mesh and bodies to mshow

--- matlab/mshow.m
+++ matlab/mshow.m
@@ -5,6 +5,11 @@ function mshow(varargin)
 args = '';
 for i = 1:nargin
     quantityName = varargin{i};
+    
+    if lower(quantityName) == "mesh" || lower(quantityName) == "bodies" || lower(quantityName) == "residuum"
+        continue
+    end
+    
     filePath = [outputPath, quantityName, '.txt'];
     if ~exist(filePath, 'file')    
         args = [args,' ', quantityName];
@@ -16,8 +21,8 @@ if args ~= ""
 end
 
 par = loadParam(simulPath);
-vertices = load([outputPath,'vertices.txt']);
-if par.dimension ~= 2 && size(vertices,2) ~= 2
+
+if par.dimension ~= 2
     error('mshow supports only 2D view');
 end
 elements = dlmread([meshPath, 'elements.txt'], ' ');
@@ -33,10 +38,23 @@ for k = 1 : nargin
         continue
     end
     
+    switch lower(q)
+        case "mesh"
+            showMesh;
+            continue        
+        case "bodies"
+            showBodies;
+            continue        
+        case "residuum"
+            showResiduum;
+            continue
+    end
+    
     plotMe(q)
 end
 
 function plotMe(quantityName)
+    vertices = load([outputPath,'vertices.txt']);
     filePath = [outputPath, quantityName, '.txt'];
     
     if ~exist(filePath, 'file')
@@ -86,4 +104,103 @@ function tri = convert2Triangular(elements, elementType)
     for i = 1 : length(squares)
         tri(nElems+i, :) = [squares(i,1) squares(i,3) squares(i,4)];
     end    
+end
+
+function showMesh(xy, elems, neigh, type)
+if nargin == 0
+    [meshPath, ~, ~] = getPath;    
+
+    xy = dlmread([meshPath, 'vertices.txt']);    
+    elems = dlmread([meshPath, 'elements.txt'])+1;
+    neigh = dlmread([meshPath, 'neighbors.txt']);
+    type = dlmread([meshPath, 'elementType.txt']);
+
+    fprintf('sit ma %d elementu\n', length(elems));
+end
+
+x = xy(:,1);
+y = xy(:,2);
+
+tri = convert2Triangular(elems, type);
+
+figure('color','w','name', 'mesh')
+triplot(tri, xy(:,1), xy(:,2), 'k', 'linewidth', .1);
+
+hold on
+color = 'bgrm';
+linewidth = 2;
+for i = 1 : size(elems,1)
+    for j = 1 : type(i)                        
+        if neigh(i,j) < 0
+            jp = mod(j, type(i)) + 1;
+            ind = [elems(i,j),elems(i,jp)];
+
+            plot(x(ind),y(ind),'color',color(-neigh(i,j)),'linewidth',linewidth);
+        end
+    end
+end
+
+axis equal
+box on
+
+end
+
+function showBodies
+
+[meshPath, ~, ~] = getPath;    
+
+xy = dlmread([meshPath, 'vertices.txt']);    
+ale = dlmread([meshPath, 'boundaryTypeALE.txt']);
+
+color = 'brkmcgkybrkmcgkybrkmcgky';
+
+figure('color','w','name', 'bodies')
+hold on
+linewidth = 2;
+for i = 1 : length(ale)
+    inds = ale(i,2:3)+1;
+    plot(xy(inds,1), xy(inds,2), 'color', color(ale(i,1)), 'linewidth', linewidth)
+end
+axis equal
+box on
+
+end
+
+function showResiduum
+
+[~, simulPath, ~] = getPath;
+residuum = load([simulPath,'residuum.txt']);
+
+linewidth = 2;
+
+figure('color','w','name','residuum')
+semilogy(residuum(:,1),'color','k','linewidth',linewidth)
+grid on
+box on
+ylabel('resid')
+xlabel('iteration')
+
+figure('color','w','name','residuum')
+semilogy(residuum(:,2),residuum(:,1),'color','k','linewidth',linewidth)
+grid on
+box on
+ylabel('resid')
+xlabel('time')
+
+figure('color','w','name','residuum')
+semilogy(reorganise(residuum(:,3)/1000),residuum(:,1),'color','k','linewidth',linewidth)
+grid on
+box on
+ylabel('resid')
+xlabel('CPU [s]')
+
+function x = reorganise(x)
+n = length(x);
+for i = 2:n
+    if(x(i) < x(i-1))
+        x(i:end) = x(i:end) + x(i-1)-x(i);
+    end
+end
+end
+
 end
