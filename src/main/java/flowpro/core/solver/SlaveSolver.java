3af441266a8a35c22a72ab071ff3d3bf23d59a38
ondrabublik <obublik@ntis.zcu.cz>
2020-01-17 14:12:35 +0100

    Fix backslashes in manifest

--- src/main/java/flowpro/core/solver/SlaveSolver.java
+++ src/main/java/flowpro/core/solver/SlaveSolver.java
@@ -15,21 +15,18 @@ import litempi.MPIException;
 abstract public class SlaveSolver {
 
     abstract public void solve() throws MPIException, IOException;
-    
-    public static SlaveSolver factory(String parallelSolverType, String masterIP, int masterPort) {
 
-        try {
-            switch (parallelSolverType.toLowerCase()) {                
-                case "schwartz":
-                    return new SchwartzImplicitSolverSlave(masterIP, masterPort);
-                case "ksp":
-                    return new KSPSolverSlave(masterIP, masterPort);
-                default:
-                    System.out.println("unknown slave solver " + parallelSolverType);
-            }
-        } catch (IOException | MPIException ex) {
-            ex.printStackTrace();
+    public static SlaveSolver factory(String parallelSolverType, String masterIP, int masterPort) throws IOException, MPIException {
+
+        switch (parallelSolverType.toLowerCase()) {
+            case "schwartz":
+                return new SchwartzImplicitSolverSlave(masterIP, masterPort);
+            case "ksp":
+                return new KSPSolverSlave(masterIP, masterPort);
+            default:
+                System.out.println("unknown slave solver " + parallelSolverType);
         }
+
         return null;
     }
 }
